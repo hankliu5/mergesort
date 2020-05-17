85344ae1750bb6e0038d50275f36279776d7ba34
hankliu5 <yliu719@ucr.edu>
2020-05-17 15:01:04 -0700

    separate library from main script

--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,7 @@ use rand;
 use std::env;
 use std::process;
 
-fn merge(left: &[i32], right: &[i32], v: &mut [i32]) {
-    let mut left_i = 0;
-    let mut right_i = 0;
-    let mut index = 0;
-
-    while left_i < left.len() && right_i < right.len() {
-        if left[left_i] <= right[right_i] {
-            v[index] = left[left_i];
-            left_i += 1;
-        } else {
-            v[index] = right[right_i];
-            right_i += 1;
-        }
-        index += 1;
-    }
-
-    if left_i < left.len() {
-        v[index..].copy_from_slice(&left[left_i..]);
-    } else {
-        v[index..].copy_from_slice(&right[right_i..]);
-    }
-}
-
-fn mergesort(v: &mut [i32]) {
-    let mid = v.len() / 2;
-    if mid == 0 {
-        return;
-    }
-    mergesort(&mut v[..mid]);
-    mergesort(&mut v[mid..]);
-
-    let mut ret = v.to_vec();
-    merge(&v[..mid], &v[mid..], &mut ret[..]);
-    v.copy_from_slice(&ret);
-}
+mod mergesort;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -59,7 +25,7 @@ fn main() {
     valid_vector.sort();
 
     // recursive sorting
-    mergesort(&mut input_vector[..]);
+    mergesort::mergesort(&mut input_vector[..]);
 
     // validate the answer
     assert_eq!(input_vector, valid_vector);
---
+++ src/mergesort.rs
@@ -0,0 +1,35 @@
+fn merge(left: &[i32], right: &[i32], v: &mut [i32]) {
+    let mut left_i = 0;
+    let mut right_i = 0;
+    let mut index = 0;
+
+    while left_i < left.len() && right_i < right.len() {
+        if left[left_i] <= right[right_i] {
+            v[index] = left[left_i];
+            left_i += 1;
+        } else {
+            v[index] = right[right_i];
+            right_i += 1;
+        }
+        index += 1;
+    }
+
+    if left_i < left.len() {
+        v[index..].copy_from_slice(&left[left_i..]);
+    } else {
+        v[index..].copy_from_slice(&right[right_i..]);
+    }
+}
+
+pub fn mergesort(v: &mut [i32]) {
+    let mid = v.len() / 2;
+    if mid == 0 {
+        return;
+    }
+    mergesort(&mut v[..mid]);
+    mergesort(&mut v[mid..]);
+
+    let mut ret = v.to_vec();
+    merge(&v[..mid], &v[mid..], &mut ret[..]);
+    v.copy_from_slice(&ret);
+}
