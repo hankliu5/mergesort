991a17b3d12fcb69d23d0feebecfdc1b8c123775
hankliu5 <yliu719@ucr.edu>
2020-05-17 11:47:31 -0700

    add input vector and generate random values

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use rand;
+
 fn main() {
     println!("Hello, world!");
+    let input_size = 16;
+    // create a random input integer array (could be generic in the end)
+    let mut input_vector: Vec<i32> = Vec::with_capacity(input_size);
+
+    for _ in 0..input_vector.capacity() {
+        input_vector.push(rand::random());
+    };
+
+    // recursive sorting
+
+    // validate the answer
+    for num in &input_vector {
+        print!("{} ", num);
+    }
+    println!();
 }
